c9d678d2b896d94114b40f8d921346cfb55d4348
namratapanga12 <43778304+namratapanga12@users.noreply.github.com>
2020-04-21 13:42:21 +0530

    Add files via upload

---
+++ computeCost.py
@@ -0,0 +1,12 @@
+import numpy as np
+def computeCost(X,y,theta):
+    """
+    Take in a numpy array X,y, theta and generate the cost function using theta as parameter in a linear regression model
+    """
+    # np.dot(A,B) => gives dot product of A and B
+    # np.power(A,n) => returns array with each element raised to the power n
+    # np.sum(A) => Returns scalar with every element in A summed up
+    m=len(y)
+    predictions=np.dot(X,theta)
+    square_err=(predictions-y)**2
+    return 1/(2*m) * np.sum(square_err)
